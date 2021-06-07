bf49bfd98a5c63891d8c9abe7604921bf127a34b
Marika-Kankia <MarikaKankia777@gmail.com>
2021-06-07 18:16:55 +0400

    Update main.js

--- main.js
+++ main.js
@@ -11,21 +11,24 @@ function integer(a){
 //2.დაწერეთ ფუნქცია, რომელიც პარამეტრად მიიღებს ორ რიცხვს - მართკუთხედის გვერდების ზომებს და დააბრუნებს მართკუთხედის ფართობს.
 
 
-function Area(lenght,width){
-	return lenght*width;
+function Area(a,b){
+	if(a > 0 && b > 0){
+	return a*b;
+  }
 
 };
 
-let lenght=4;
-let width=7;
-console.log('Area is' + Area(lenght,width));
+let a=4;
+let b=7;
+console.log('Area is' + Area(a,b));
 
 //3.დაწერეთ ფუქცია, რომელიც პარამეტრად მიიღებს ერთ რიცხვს - კვადრატის გვერდის სიგრძეს და დააბრუნებს კვადრატის ფართობს. ფართობის გამოსათვლელად გამოიყენეთ #2 პუნქტში აღწერილი ფუნქცია.
 
 
 function AreaOfSquare(d){
+	if(d>0){
 	return d*d;
-
+  }
 };
 
 let d=6;
@@ -49,12 +52,20 @@ function getRandom(a,b) {
 
 //6.დაწერეთ ფუნქცია, რომელიც პარამეტრად მიიღებს სტრინგს: "boy" ან "girl" და დააბრუნებს 10 გოგოს ან ბიჭის სახელიდან ერთ-ერთს შემთხვევითად. (სახელები ჩაწერილი იქნება მასივში ამავე ფუნქციაში. კარგი იქნება გამოიყენოთ #5 პუნქტში დაწერილი ფუნქცია).
 
-// function list() {
+
+// function list(Y){
 //     let girlsnames= ['Lika','Salome','Lana','Maka','Tamuna','Elena','Ana','Tika','Megi','Ia'];
 //     let boysnames = ['Nika','Giorgi','Vaso','Lasha','Oto','Tornike','Levani','Luka','Makho','Erekle'];
 //     let girlsrandom=Math.floor(Math.random()*girlsnames.lenght);
+//     let boysrandom=Math.floor(Math.random()*boysnames.lenght);
+//     if(Y=='girl'){
+//     	return girlsnames[Math.floor(Math.random()*girlsnames.lenght)];
+//     };
   
-// };
+//     if(Y=='boy'){
+//     	return boysnames[Math.floor(Math.random()*boysnames.lenght)];
+//     };
+  
 
 
 
@@ -73,16 +84,15 @@ function isLeapYear(year) {
 
 
 function volume(m,n,h){
+	if (a > 0  && b > 0 && c > 0) {
 	return m*n*h;
+   }
 }
 let m=2,n=4,h=5;
 console.log('volume is'+ volume(m,n,h));
 
-//9.დაწერეთ რეკურსიული ფუნქცია, რომელიც პარამეტრად მიიღებს ორ რიცხვს (a, b), რეკურსიის გამოყენებით შეამოწმებს ყველა რიცხვს a დან b მდე და დალოგავს მხოლოდ კენტ რიცხვებს. (გამოიყენეთ #1 პუნქტში დაწერილი ფუნქცია)
 
-
-
-//10 დაწერეთ რეკურსიული ფუნქცია, რომელიც პარამეტრებად მიიღებს ორ რიცხვს (x, y) და დააბრუნებს x ხარისხად y -ს. მაგალითად თუ ფუნქციამ მიიღო პარამეტრად 2 და 5 უნდა დააბრუნოს 2 -ს მეხუთე ხარისხი ანუ 32. გამოიყენეთ რეკურსია.
+//10* დაწერეთ რეკურსიული ფუნქცია, რომელიც პარამეტრებად მიიღებს ორ რიცხვს (x, y) და დააბრუნებს x ხარისხად y -ს. მაგალითად თუ ფუნქციამ მიიღო პარამეტრად 2 და 5 უნდა დააბრუნოს 2 -ს მეხუთე ხარისხი ანუ 32. გამოიყენეთ რეკურსია.
 
 
 
@@ -96,6 +106,6 @@ let power = function(x,y) {
     }
 
     return x * power(x, y-1);
-}
+};
    let x=2,y=3;
    console.log(power(x,y));
